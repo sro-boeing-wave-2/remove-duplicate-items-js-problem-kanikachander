c5291b30757f8a07c0e5ff98315a3b085da8029b
Kanika <kanikachander1@gmail.com>
2018-07-10 15:39:23 +0530

    remove-duplicate-items-js-problem

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {};
+const removeDuplicateItems = (items) => {
+  const mySet = new Set(items);
+  const array = Array.from(mySet);
+  return array;
+};
 
 module.exports = removeDuplicateItems;
